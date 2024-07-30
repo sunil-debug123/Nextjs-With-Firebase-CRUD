93d686798f3c0c7b10371e3529afea3c6f705858
Sunil_debug123 <sunil32715@gmail.com>
2024-07-30 15:12:57 +0530

    Ready to Deploy

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,24 +1,34 @@
-
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
 import Loader from './Loader';
 import { useAuth } from '@/firebase/auth';
-import { useRouter } from 'next/navigation';
 
-const ProtectedRoute = ({ children }) => {
-  const { authUser, isLoading } = useAuth();
-  const router = useRouter();
-  {console.log({authUser})}
-  useEffect(() => {
-    if (!isLoading && !authUser) {
-      router.push('/login');
+const ProtectedRoute = (WrappedComponent) => {
+  return (props) => {
+    const { authUser, isLoading } = useAuth();
+    const router = useRouter();
+    const pathname = usePathname();
+    const [redirecting, setRedirecting] = useState(true); // State to handle redirection
+
+    useEffect(() => {
+      // Handle redirection based on auth status
+      if (!isLoading) {
+        if (authUser && (pathname === '/login' || pathname === '/signup')) {
+          router.push('/dashboard'); // Redirect to the dashboard
+        } else if (!authUser && pathname !== '/login' && pathname !== '/signup') {
+          router.push('/login'); // Redirect to login
+        } else {
+          setRedirecting(false); // Stop redirecting if conditions are met
+        }
+      }
+    }, [authUser, isLoading, pathname, router]);
+
+    if (isLoading || redirecting) {
+      return <Loader />;
     }
-  }, [authUser, isLoading]);
 
-  if (isLoading || !authUser) {
-    return <div><Loader /></div>;
-  }
-
-  return children;
+    return <WrappedComponent {...props} />;
+  };
 };
 
 export default ProtectedRoute;
